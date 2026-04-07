19cd9ab704bb910aaf94286f09e436f77315b70d
L33T <271813410+chiefbigrubberduck@users.noreply.github.com>
2026-04-07 17:58:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name             = "decrypt",
     version          = "0.1.2",
-    description      = "Pipe programs through decrypt to make your boss think you are l33t",
+    description      = "Pipe programs through decrypt to fool people into thinking you are a hacker",
     long_description = open('README.md').read(),
-    author           = "jtwaleson",
-    url              = "https://github.com/jtwaleson/decrypt",
+    author           = "L33T",
+    url              = "None",
     packages         = ["decrypt"],
     entry_points     = {'console_scripts': ['decrypt = decrypt.decrypt:main']},
 )
